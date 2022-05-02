5a9bd9f8bb0b05a9ddfa713a88a02c39dfe2aa4d
AshvathMurthy <92076903+AshvathMurthy@users.noreply.github.com>
2022-05-02 10:17:33 +0530

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,36 @@
+console.log("hello,world");
+
+console.log(typeof nan)
+
+
+
+var msg = "eligible";
+
+function drivingTest(age) {
+    let msg;
+    if (age > 18) {
+       msg = "eligible";
+    } else {
+       msg = "not eligible";
+    }
+    console.log("💂🏽‍♀️ are " + msg);
+  }
+  
+  drivingTest(20);
+
+
+
+  
+
+
+
+
+console.log(
+   ((function(rollno)){
+      var firstname ="ashed";
+      let lastname ="snm";
+      return firstname + " " + lastname + "-" + rollno
+   }((21))
+   
+
+);
---
+++ this-keyword.js
@@ -0,0 +1,10 @@
+var introMsg= fuction (state, country) {
+    return $(this,lastNmae), $(this,firstNmae) is from $(state), $(country)!!!!!!!!
+};
+
+var students={
+    firstName="ash"
+    lastName="k"
+};
+
+console.log(introMsg.call9students,"12","ind"));
